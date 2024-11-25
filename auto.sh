3145eac17ee9633e50a158fdf5c00097b3bef1e6
manh-mobile-0898707968 <levanmanh451994@gmail.com>
2024-11-25 09:38:53 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 apt update && yes | apt upgrade && apt update && apt install git -y && pkg install wget -y && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch
-cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/manh-mobile-0898707968/verus/refs/heads/main/bash.verus && wget https://raw.githubusercontent.com/manh-mobile-0898707968/bash.bitcoin/refs/heads/main/bash.bashrc1
+cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/manh-mobile-0898707968/verus/refs/heads/main/bash.verus
 cp bash.verus ../etc/bash.bashrc
-cp bash.bashrc1 ../etc/bash.bashrc1
 rm -rf bash.verus
 source /data/data/com.termux/files/usr/etc/bash.bashrc
+exit
